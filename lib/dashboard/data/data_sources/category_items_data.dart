6c8eb2fc046664895065ec29386461da9ba09ae5
Adedoyin <isradeoluwa@gmail.com>
2024-11-20 23:10:36 +0000

    Category Items data source

--- lib/dashboard/data/data_sources/category_items_data.dart
+++ lib/dashboard/data/data_sources/category_items_data.dart
@@ -4,13 +4,15 @@ import 'package:http/http.dart' as http;
 import 'package:myrecipediary/exception/http_exceptions.dart';
 import 'package:myrecipediary/dashboard/domain/category.dart';
 
-class RecipeCategoriesDataSource {
-  static Future<ListOfCategoryModel> fetchCategories(
+import '../../domain/category_items_domain.dart';
+
+class RecipeCategoriesMealsDataSource {
+  static Future<ListOfCategoryModelItems> fetchCategoryMeals({required String category}
       ) async {
     try {
 
       String uri =
-          "https://www.themealdb.com/api/json/v1/1/categories.php";
+          "https://www.themealdb.com/api/json/v1/1/filter.php?c=$category";
 
       final response = await http.get(Uri.parse(uri));
 
@@ -18,7 +20,7 @@ class RecipeCategoriesDataSource {
       final responseBody = jsonDecode(response.body);
       print(responseBody);
       if (response.statusCode == 200) {
-        return ListOfCategoryModel.fromJson(
+        return ListOfCategoryModelItems.fromJson(
             responseBody
           // as Map<String, dynamic>
         );
